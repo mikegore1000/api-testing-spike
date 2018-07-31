05930dc86aebfee622b5bee4dd45091ff74338a2
Mike Gore <mikegore1000@msn.com>
2018-07-31 21:21:33 +0100

    Added test to check a custom API factory can have it's config overridden

--- Api.Tests/TestControllerTests.cs
+++ Api.Tests/TestControllerTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,11 +16,27 @@ namespace Api.Tests
             var factory = new WebApplicationFactory<Startup>();
             var anotherFactory = factory.WithWebHostBuilder(builder =>
             {
-                //builder.ConfigureServices(services =>
-                //{
-                //    services.AddSingleton<IGreetingService, FakeGreetingService>();
-                //});
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddSingleton<IGreetingService, FakeGreetingService>();
+                });
+            });
 
+            var client = anotherFactory.CreateClient();
+
+            var result = await client.GetStringAsync("/api/test");
+
+            Assert.Equal("Fake", result);
+        }
+
+
+
+        [Fact]
+        public async Task TestWithApiFactory()
+        {
+            var factory = new ApiFactory();
+            var anotherFactory = factory.WithWebHostBuilder(builder =>
+            {
                 builder.ConfigureTestServices(services =>
                 {
                     services.AddSingleton<IGreetingService, FakeGreetingService>();
@@ -34,11 +51,30 @@ namespace Api.Tests
         }
     }
 
+    public class ApiFactory : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddSingleton<IGreetingService, AnotherFakeGreetingService>();
+            });
+        }
+    }
+
     public class FakeGreetingService : IGreetingService
     {
         public string GetGreeting()
         {
             return "Fake";
+        }
+    }
+
+    public class AnotherFakeGreetingService : IGreetingService
+    {
+        public string GetGreeting()
+        {
+            return "Another Fake";
         }
     }
 }
